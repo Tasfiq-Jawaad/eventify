cf44e0718c664ca1388cd873d5c013d23d27cd91
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2024-05-09 04:40:48 +0100

    function added to update event details from local storage

--- edit.js
+++ edit.js
@@ -2,6 +2,39 @@ $(document).ready(function () {
     let eventID = getQueryParam() || null;
 
     getEventDetails(eventID);
+
+    $("#eventForm").submit(function (event) {
+        event.preventDefault(); // Prevent page form reloading
+
+        // Retrieve event data from local storage
+        let eventData = localStorage.getItem("eventData");
+        if (eventData) {
+            // Parse event data
+            eventData = JSON.parse(eventData);
+
+            // Check if the index is within the array bounds
+            if (eventID >= 0 && eventID < eventData.length) {
+                // Update event details with form values
+                eventData[eventID].title = event?.target.title?.value;
+                eventData[eventID].host = event?.target?.host?.value;
+                eventData[eventID].description = event?.target?.description?.value;
+                eventData[eventID].location = event?.target?.isOnline?.checked ? "Online" : event?.target?.location?.value;
+                eventData[eventID].fee = event?.target?.isFree?.checked ? 0 : event?.target?.fee?.value;
+                eventData[eventID].isFree = event?.target?.isFree?.checked;
+                eventData[eventID].isOnline = event?.target?.isFree?.checked;
+
+                // Store updated event data back into local storage
+                localStorage.setItem("eventData", JSON.stringify(eventData));
+
+                alert("Event details have been updated successfully!");
+            } else {
+                alert("Invalid event ID.");
+            }
+        } else {
+            // Display message if there's no event data in local storage
+            alert("No event details available.");
+        }
+    });
 });
 
 const getEventDetails = (eventID) => {
--- post.js
+++ post.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    $("#getLocationButton").click(getUserLocation);
-
     $("#eventForm").submit(function (event) {
         event.preventDefault(); // Prevent page reloading
 
@@ -27,103 +25,5 @@ $(document).ready(function () {
         localStorage.setItem("eventData", JSON.stringify(eventDataArray));
 
         alert("Event data has been stored in local storage!");
-    });
-});
-
-
-const getUserLocation = () => {
-    // Check if geolocation is supported by the browser
-    if ("geolocation" in navigator) {
-        // Get the user's current position
-        navigator.geolocation.getCurrentPosition(async function (position) {
-            // Access the latitude and longitude
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-
-            console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-
-            getAddress(latitude, longitude);
-
-        }, function (error) {
-            // Handle errors
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    console.error("User denied the request for geolocation.");
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    console.error("Location information is unavailable.");
-                    break;
-                case error.TIMEOUT:
-                    console.error("The request to get user location timed out.");
-                    break;
-                case error.UNKNOWN_ERROR:
-                    console.error("An unknown error occurred.");
-                    break;
-            }
-        });
-    } else {
-        console.error("Geolocation is not supported by this browser.");
-    }
-}
-
-const getAddress = (lat, lng) => {
-
-    // fetch the formatted address
-    fetch(`https://nodejs-serverless-function-express-liard-iota-73.vercel.app/get-address?lat=${lat}&lng=${lng}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch data from server');
-            }
-            return response.json();
-        })
-        .then(data => {
-
-            console.log('Formatted address:', data?.results[0]?.formatted_address);
-
-            // display the formatted address
-            data?.results[0]?.formatted_address ?
-                displayFormattedAddress(data?.results[0]?.formatted_address)
-                :
-                displayAddressWarningMessage("Can not access the location")
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-const displayFormattedAddress = (address) => {
-    $(document).ready(function () {
-
-        $("#location").val(address);
-        displayAddressWarningMessage("Note: address might not be 100% accurate");
-    });
-}
-
-const displayAddressWarningMessage = (message) => {
-    $(document).ready(function () {
-
-        $("#warningMessage").remove();
-        $("#locationInput").append(`<div id="warningMessage">${message}</div>`)
-    });
-}
-
-
-$(document).ready(function () {
-    $("#isFree").change(function () {
-        if (this.checked) {
-            $("#fee").val(0);
-            $("#fee").prop("disabled", true);
-        } else {
-            $("#fee").prop("disabled", false);
-        }
-    });
-
-    $("#isOnline").change(function () {
-        if (this.checked) {
-            $("#location").val("Online");
-            $("#location").prop("disabled", true);
-        } else {
-            $("#location").prop("disabled", false);
-        }
     });
 });
