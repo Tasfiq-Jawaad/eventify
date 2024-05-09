926cb999d0921a77bfb939a50b009db45cdccf9e
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2024-05-09 01:24:01 +0100

    event form saving to local storage

--- post.js
+++ post.js
@@ -3,6 +3,32 @@ import { getAddress } from "./utility.js";
 $(document).ready(function () {
 
     $("#getLocationButton").click(getUserLocation);
+
+    $("#eventForm").submit(function (event) {
+        event.preventDefault(); // Prevent page reloading
+
+        const existingData = localStorage.getItem("eventData");
+        const eventDataArray = existingData ? JSON.parse(existingData) : [];
+
+        // Get form data
+        const formData = {
+            title: $("#title").val(),
+            description: $("#description").val(),
+            location: $("#isOnline").prop("checked") ? "Online" : $("#location").val(),
+            price: $("#isFree").prop("checked") ? 0 : $("#fee").val(),
+            isFree: $("#isFree").prop("checked"),
+            isOnline: $("#isOnline").prop("checked")
+        };
+
+        console.log("Form data", formData)
+
+        eventDataArray.push(formData);
+
+        // Store updated data in local storage
+        localStorage.setItem("eventData", JSON.stringify(eventDataArray));
+
+        alert("Event data has been stored in local storage!");
+    });
 });
 
 
